58b414a70d8dd11a257c6cdcc530915ebb54ad54
Guilherme de Oliveira <oliveira.gui5@gmail.com>
2020-12-30 03:01:45 -0300

    function name change

--- main/main.c
+++ main/main.c
@@ -41,20 +41,21 @@ void timer_callback(void *arg);
 void button_callback(void *arg);
 
 //interrupt handlers
-void IRAM_ATTR gpio_isr_handler(void *arg);
+static void IRAM_ATTR gpio_isr_handler(void *arg);
 
 //config functions
-void gpio_input_config(gpio_num_t pin);
+void setup_gpio(gpio_num_t pin);
 
 void app_main(void)
 {
+
+   gpio_device_config(GPIO_NUM_14);
    ir_tx_queue = xQueueCreate(1, sizeof(MideaFrameData));
    xTaskCreate(task_heart_beat, "heart-beat", 2048, NULL, 5, &heart_beat_task_handle);
-   xTaskCreate(task_tx,"TX-task",1024,NULL,10,&tx_task_handle);
+   xTaskCreate(task_tx, "TX-task", 4096, NULL, 10, &tx_task_handle);
 
    tmr = xTimerCreate("heart-beat-timer", pdMS_TO_TICKS(1000), true, NULL, timer_callback);
    xTimerStart(tmr, pdMS_TO_TICKS(100));
-   gpio_input_config(GPIO_NUM_14);
 }
 
 void timer_callback(void *arg)
@@ -74,35 +75,60 @@ void task_heart_beat(void *arg)
    }
 }
 
-void IRAM_ATTR gpio_isr_handler(void *arg)
+static void IRAM_ATTR gpio_isr_handler(void *arg)
 {
-   xQueueSendFromISR(ir_tx_queue, NULL, NULL);
+   MideaFrameData data;
+   xQueueSendFromISR(ir_tx_queue, &data, NULL);
 }
 
-void gpio_input_config(gpio_num_t pin)
+void setup_gpio(gpio_num_t pin)
 {
    gpio_config_t io_conf;
+   //disable interrupt
+   io_conf.intr_type = GPIO_INTR_DISABLE;
+   //set as output mode
+   io_conf.mode = GPIO_MODE_OUTPUT;
+   //bit mask of the pins that you want to set,e.g.GPIO18/19
+   io_conf.pin_bit_mask = (1ULL << GPIO_NUM_2);
+   //disable pull-down mode
+   io_conf.pull_down_en = 0;
+   //disable pull-up mode
+   io_conf.pull_up_en = 0;
+   //configure GPIO with the given settings
+   gpio_config(&io_conf);
+
    //interrupt of rising edge
    io_conf.intr_type = GPIO_INTR_POSEDGE;
    //bit mask of the pins, use GPIO4/5 here
-   io_conf.pin_bit_mask = pin;
+   io_conf.pin_bit_mask = (1ULL << pin);
    //set as input mode
    io_conf.mode = GPIO_MODE_INPUT;
-   //enable pull-up mode
+   //enable pull-up mode 
    io_conf.pull_up_en = 1;
    gpio_config(&io_conf);
+
+   //change gpio intrrupt type for one pin
+   gpio_set_intr_type(pin, GPIO_INTR_ANYEDGE);
 
    //install gpio isr service
    gpio_install_isr_service(0);
    //hook isr handler for specific gpio pin
-   gpio_isr_handler_add(pin, gpio_isr_handler, (void *)pin);
+   gpio_isr_handler_add(pin, gpio_isr_handler, NULL);
+
+   //remove isr handler for gpio number.
+   gpio_isr_handler_remove(GPIO_NUM_2);
+   //hook isr handler for specific gpio pin again
+   gpio_isr_handler_add(pin, gpio_isr_handler, NULL);
 }
 
-void task_tx(void *arg){
-   for(;;){
-
-      if(xQueueReceive(ir_tx_queue, NULL, portMAX_DELAY)) {
-             ESP_LOGI("tx-task", "gpio-evt-received");
-        }
+void task_tx(void *arg)
+{
+   MideaFrameData data;
+   for (;;)
+   {
+      if (xQueueReceive(ir_tx_queue, &data, portMAX_DELAY))
+      {
+         ESP_LOGI("tx-task", "gpio-evt-received");
+      }
    }
 }
